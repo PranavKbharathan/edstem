caded1f1e79c0b157e3c10e4462eec43d228921f
unknown <pranav.k.bharathan@gmail.com>
2025-06-03 16:14:24 +0530

    added pagination and change the bloc concept

--- lib/features/home/presentation/bloc/home_event.dart
+++ lib/features/home/presentation/bloc/home_event.dart
@@ -3,6 +3,4 @@ part of 'home_bloc.dart';
 @freezed
 class HomeEvent with _$HomeEvent {
   const factory HomeEvent.searchMovie(String name,bool isNextPage) = _SearchMovie;
-
-  const factory HomeEvent.loadMore(bool isNextPage) = _LoadMore;
 }
